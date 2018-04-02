532b5aa09be03c10d51f99bb73a4ae73cd2584bd
IMT-SOFT\anndh <anndh@imt-soft.com>
2018-04-02 22:03:40 +0700

    + add WCF

---
+++ Project_QLPK/Project_DAL/DrugDAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_DAL
+{
+    public class DrugDAL
+    {
+        DataBaseDataContext db;
+        public DrugDAL()
+        {
+            db = new DataBaseDataContext();
+        }      
+        public List<Drug> getAllDrug()
+        {
+            var ls = db.Drugs.ToList();
+            return db.Drugs.ToList();
+        }
+        public Drug getOneDrug(int Id)
+        {
+            return db.Drugs.Where(x => x.DrugID == Id).FirstOrDefault();
+        }
+        public void insertDrug(Drug Drug)
+        {
+            db.Drugs.InsertOnSubmit(Drug);
+            db.SubmitChanges();
+        }
+        public void updateDrug(Drug Drug)
+        {
+            //db.Drugs.     
+            //db.SubmitChanges();
+        }   
+    }
+}
---
+++ Project_QLPK/Project_DAL/PatientOfDayDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_DAL
+{
+    public class PatientOfDayDAL
+    {       
+        DataBaseDataContext db;
+        public PatientOfDayDAL()
+        {
+            db = new DataBaseDataContext();
+        }
+        public List<PartientOfDay> getAllPartientOfDay()
+        {
+            return db.PartientOfDays.ToList();
+        }
+        public PartientOfDay getOnePartientOfDay(int Id)
+        {
+            return db.PartientOfDays.Where(x => x.PartientID == Id).FirstOrDefault();
+        }
+        public void insertPartientOfDay(PartientOfDay PartientOfDay)
+        {
+            db.PartientOfDays.InsertOnSubmit(PartientOfDay);
+            db.SubmitChanges();
+        }
+        public void updatePartientOfDay(PartientOfDay PartientOfDay)
+        {
+            var record = db.PartientOfDays.Where(x => x.PartientID == PartientOfDay.PartientID).FirstOrDefault();
+            record.Number = PartientOfDay.Number;
+            record.RoomID=PartientOfDay.RoomID;
+            db.SubmitChanges();
+        }
+    }
+}
